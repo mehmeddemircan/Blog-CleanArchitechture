9ca160b839a29db74de37c2f31d5d36df44f7ee4
mehmet demircan <mehmeddemircan@gmail.com>
2024-07-09 02:34:23 +0300

    added update tag command validator

---
+++ src/blogHaven/Application/Features/Tags/Validations/UpdateTagCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.Constants;
+using Application.Features.Tags.Commands.UpdateTag;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Tags.Validations
+{
+    public class UpdateTagCommandValidator : AbstractValidator<UpdateTagCommand>
+    {
+        public UpdateTagCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage(ValidationMessages.TagNameCanNotBeEmpty);
+            RuleFor(c => c.Name).MinimumLength(2).WithMessage(ValidationMessages.TagNameMinLength);
+        }
+    }
+}
